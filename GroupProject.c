4648474d45334ae6b2e302b6f22a29169aad56fe
Gabrielle Merriken <47796367+gabbumms@users.noreply.github.com>
2019-11-30 20:56:55 -0500

    Basics of User Interface
    Created menu and variable to store selection choice

--- GroupProject.c
+++ GroupProject.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
 
 int main() {
+  int choice;
 
-  printf("Welcome to your wishlist");
+
+  while(1){
+
+  printf("Welcome to your wishlist\n");
+  printf("------------------------\n");
+  printf("Menu:\n");
+  printf("------------------------\n");
+  printf("1. Add a new list\n");
+  printf("2. Display an existing list\n");
+  printf("3. Display favorites list\n");
+  printf("4. Search for an item\n");
+  printf("5. Quit\n");
+  printf("------------------------\n");
+  printf("Selection: ");
+  scanf("%d", &choice);
+
+  if (choice == 1){
+    printf("add successful\n");
+  }
+
+  if (choice == 2){
+    printf("display an exisiting list successful\n");
+  }
+
+  if (choice == 3){
+    printf("display favorites list successful\n");
+  }
+
+  if (choice == 4){
+    printf("search for an item successful\n");
+  }
+
+  if (choice == 5){
+    break;
+  }
+
+
+  }
 }
-
-// Basic structure should be an array of structs 
-// no need for linked list, we can just set a hard limit on the number of items that can be stored
-// Structs should store useful info about items like name, keywords, etc.
-// We should have support for creating multiple lists, use nested structs for this
-// We should also have support for a favorites list. Any item from any list should be able to be marked as a favorite, making it appear on the unified favorites list.
-// Basic menu design should be similar to the following:
-/*
-Menu:
-1. Add a new list
-2. Display an existing list (submenu to display available lists, lists and their comtents should be saved to a file which is read in on startup, should have an option somewhere to add an item as a favorite)
-3. Display favorites list (should have an option to remove stuff from it)
-4. Search for an item (select an item, then use Google API to search it)
-5. Quit
-8/
