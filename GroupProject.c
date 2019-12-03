3c255ad920bccb5715ec2f736aaa47945cfb2eb2
Gabrielle Merriken <47796367+gabbumms@users.noreply.github.com>
2019-12-03 16:28:40 -0500

    Working code

--- GroupProject.c
+++ GroupProject.c
@@ -10,6 +10,20 @@
 #include <netdb.h>
 #include <curl/curl.h>
 
+struct Product
+{
+  char name[50];
+  char description[50];
+  char category[100];
+  double price;
+};
+
+void displayProduct(struct Product *product)
+{
+  printf("Name: %s\n,    Description: %s\n,    Category: %s\n,    Price: %.2lf\n\n",product->name, product->description, product->category, product->price);
+}
+
+
 char *httpResponse;
 
 struct curl_response
@@ -145,56 +159,204 @@ int doSearch(char *searchTerm)
 
 int main()
 {
-  int choice;
-  char *searchString;
+    struct Product macbook;
+    struct Product hairbrush;
+    // struct Product ps4;
+    // struct Product hydroflask;
+    // struct Product lipstick;
 
-  while (1)
-  {
+    strcpy(macbook.name, "MacBook");
+    strcpy(macbook.description, "13 inch 2019 refurbished MacBook Pro");
+    strcpy(macbook.category, "Laptop");
+    macbook.price = 1099.99;
 
-    printf("\nWelcome to your wishlist\n");
-    printf("------------------------\n");
-    printf("Menu:\n");
-    printf("------------------------\n");
-    printf("1. Add a new list\n");
-    printf("2. Display an existing list\n");
-    printf("3. Display favorites list\n");
-    printf("4. Search for an item\n");
-    printf("5. Quit\n");
-    printf("------------------------\n");
-    printf("Selection: ");
-    scanf("%d", &choice);
+    strcpy(hairbrush.name, "Hairbrush");
+    strcpy(hairbrush.description, "Brush from Rihanna's beauty line Fenty");
+    strcpy(hairbrush.category, "Beauty");
+    hairbrush.price= 45.99;
 
-    if (choice == 1)
+    // strcpy(ps4.name, "PS4");
+    // strcpy(ps4.description, "The newest sony gaming console with Uncharted 4 bundle pack");
+    // strcpy(ps4.category, "Gaming");
+    // ps4.price = 199.99;
+    //
+    // strcpy(hydroflask.name, "Hydroflask");
+    // strcpy(hydroflask.description, "Waterbottle that keeps you water cold all day.");
+    // strcpy(hydroflask.category, "Health");
+    // hydroflask.price = 45.99;
+    //
+    // strcpy(lipstick.name, "Lipstick");
+    // strcpy(lipstick.description, "Versace lipstick on sale right now!");
+    // strcpy(lipstick.category, "Beauty");
+    // lipstick.price = 99.99;
+
+
+
+
+    struct Product shoppingListOne[5];
+    // struct Product shoppingListTwo[10];
+    // struct Product shoppingListThree[10];
+    // struct Product shoppingListFour[10];
+    // struct Product shoppingListFive[10];
+
+
+    int choice;
+    char pSelection[10];
+    int listCounterOne = 0;
+
+    while(1)
     {
-      printf("add successful\n");
-    }
-    if (choice == 2)
-    {
-      /*printf("Here are the items in your existing lists: ");
-      printf("----------------------------------------- \n");
-      for (int i = 0; i < listCounterOne; i++)
-      {
-        printf("Name: %s\n,    Description: %s\n,    Category: %s\n,    Price: %.2lf\n\n", shoppingListOne[i].name, shoppingListOne[i].description, shoppingListOne[i].category, shoppingListOne[i].price);
-      }
-      printf("----------------------------------------- \n");
-      printf("display an exisiting list successful\n");
-      */
+
+        printf("Welcome to your wishlist\n");
+        printf("------------------------\n");
+        printf("Menu:\n");
+        printf("------------------------\n");
+        printf("1. Search for an item\n");
+        printf("2. Add an item from recent searches\n");
+        printf("3. Display an existing list\n");
+        printf("4. Display favorites list\n");
+        printf("5. Quit\n");
+        printf("------------------------\n");
+        printf("Selection: ");
+        scanf("%d", &choice);
+
+        if (choice == 1)
+        {
+          printf("search for an item successful\n");
+          doSearch("test");
+
+           //      printf("Please select one of the following products by typing it in.\n");
+           //      printf("------------------------\n");
+           //      displayProduct(&macbook);
+           //      displayProduct(&hairbrush);
+           //      // displayProduct(&ps4);
+           //      // displayProduct(&lipstick);
+           //      // displayProduct(&hydroflask);
+           //      printf("------------------------\n");
+           //      printf("If you are done adding to the shopping list please type in Exit.\n");
+           //      printf("------------------------\n");
+           //
+           //
+           // for(int i=0; i <=5; i++)
+           // {
+           //
+           //      printf("Type in the name of the product in exactly how it is displayed ");
+           //      scanf("%s",pSelection);
+           //
+           //          if(strcmp(pSelection, "Macbook") == 0){
+           //              shoppingListOne[i]=macbook;
+           //              printf("Added Succesfully\n");
+           //              listCounterOne++;
+           //
+           //          }
+           //          // else if (strcmp(pSelection, "Lipstick") == 0){
+           //          //      shoppingListOne[i]=lipstick;
+           //          //      printf("Added Succesfully\n");
+           //          //      listCounterOne++;
+           //          // }
+           //          // else if (strcmp(pSelection, "PS4") == 0){
+           //          //     shoppingListOne[i]=ps4;
+           //          //     printf("Added Succesfully\n");
+           //          //     listCounterOne++;
+           //          // }
+           //          // else if (strcmp(pSelection, "Hydroflask") == 0){
+           //          //      shoppingListOne[i]=hydroflask;
+           //          //      printf("Added Succesfully\n");
+           //          //      listCounterOne++;
+           //          // }
+           //          else if (strcmp(pSelection, "Hairbrush") == 0){
+           //               shoppingListOne[i]=hairbrush;
+           //               printf("Added Succesfully \n");
+           //               listCounterOne++;
+           //          }
+           //          else if (strcmp(pSelection, "Exit") == 0){
+           //              printf("Exiting");
+           //               break;
+           //          }
+           //
+           //          else {
+           //              printf("Please type in a proper product name to add to the list \n");
+           //              i--;
+           //          }
+           //  }
+          }
+
+          if (choice == 2)
+          {
+            printf("Please select one of the following products by typing it in.\n");
+            printf("------------------------\n");
+            displayProduct(&macbook);
+            displayProduct(&hairbrush);
+            printf("------------------------\n");
+            printf("If you are done adding to the shopping list please type in Exit.\n");
+            printf("------------------------\n");
+            for(int i=0; i <=5; i++)
+            {
+              printf("Type in the name of the product in exactly how it is displayed ");
+              scanf("%s",pSelection);
+
+                  if(strcmp(pSelection, "Macbook") == 0)
+                  {
+                      shoppingListOne[i]=macbook;
+                      printf("Added Succesfully\n");
+                      listCounterOne++;
+                  }
+                  // else if (strcmp(pSelection, "Lipstick") == 0){
+                  //      shoppingListOne[i]=lipstick;
+                  //      printf("Added Succesfully\n");
+                  //      listCounterOne++;
+                  // }
+                  // else if (strcmp(pSelection, "PS4") == 0){
+                  //     shoppingListOne[i]=ps4;
+                  //     printf("Added Succesfully\n");
+                  //     listCounterOne++;
+                  // }
+                  // else if (strcmp(pSelection, "Hydroflask") == 0){
+                  //      shoppingListOne[i]=hydroflask;
+                  //      printf("Added Succesfully\n");
+                  //      listCounterOne++;
+                  // }
+                  else if (strcmp(pSelection, "Hairbrush") == 0){
+                       shoppingListOne[i]=hairbrush;
+                       printf("Added Succesfully \n");
+                       listCounterOne++;
+                  }
+                  else if (strcmp(pSelection, "Exit") == 0){
+                      printf("Exiting");
+                       break;
+                  }
+
+                  else {
+                      printf("Please type in a proper product name to add to the list \n");
+                      i--;
+                  }
+                }
+              }
+
+            if (choice == 3)
+            {
+              printf("Here are the items in your existing lists: ");
+              printf("----------------------------------------- \n");
+              for(int i = 0; i<listCounterOne; i++)
+              {
+                printf("Name: %s\n,    Description: %s\n,    Category: %s\n,    Price: %.2lf\n\n",shoppingListOne[i].name, shoppingListOne[i].description, shoppingListOne[i].category, shoppingListOne[i].price);
+              }
+              printf("----------------------------------------- \n");
+              printf("display an exisiting list successful\n");
+            }
+
+            if (choice == 4)
+            {
+              printf("display favorites list successful\n");
+                // printf("search for an item successful\n");
+                // doSearch("test");
+            }
+
+            if (choice == 5)
+            {
+                break;
+            }
+
     }
 
-    if (choice == 3)
-    {
-      printf("display favorites list successful\n");
-    }
-
-    if (choice == 4)
-    {
-      printf("search for an item successful\n");
-      doSearch("test");
-    }
-
-    if (choice == 5)
-    {
-      break;
-    }
-  }
 }
