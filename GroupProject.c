087ca39c44946e3588b15a95bf1d605cff299075
Jackson Atassi <jackson.atassi@outlook.com>
2019-12-03 11:33:18 -0500

    searching works now

--- GroupProject.c
+++ GroupProject.c
@@ -12,68 +12,54 @@
 
 char* httpResponse;
 
-struct string
-{
+struct curl_response{
   char *text;
-  size_t length;
+  size_t size;
 };
 
-void init_string(struct string *s) {
-  s->length = 0;
-  s->text = malloc(s->length+1);
-  if (s->text == NULL) {
-    fprintf(stderr, "malloc() failed\n");
-    exit(EXIT_FAILURE);
+size_t curl_callback (void *conents, size_t size, size_t nmemb, void *userp)
+{
+  size_t realsize = size * nmemb;
+  struct curl_response *p = (struct curl_response *)userp;
+
+  p->text = (char*)realloc(p->text, p->size + realsize + 1);
+
+  if (p->text == NULL)
+  {
+    printf("\nCouldn't expand buffer in curl_callback.");
+    free(p->text);
+    return -1;
   }
-  s->text[0] = '\0';
-  printf("\ninit_string is chill.");
+
+  memcpy(&(p->text[p->size]), conents, realsize);
+
+  p->size += realsize;
+  p->text[p->size] = 0;
+
+  return realsize;
 }
 
-size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string *s)
+CURLcode curl_fetch(CURL *c, char *url, struct curl_response *response)
 {
-  size_t new_len = s->length + size*nmemb;
-  s->text = realloc(s->text, new_len+1);
-  if (s->text == NULL) {
-    fprintf(stderr, "realloc() failed\n");
-    exit(EXIT_FAILURE);
-  }
-  memcpy(s->text+s->length, ptr, size*nmemb);
-  s->text[new_len] = '\0';
-  s->length = new_len;
-
-  printf("\nwritefunc is chill.");
-
-  return size*nmemb;
-}
-
-char* sendRequest(char* requestURI)
-{
-  CURL *curl;
   CURLcode result;
 
-  curl = curl_easy_init();
-  if (curl)
+  response->text = (char*)calloc(1, sizeof(response->text));
+
+  if (response->text == NULL)
   {
-    struct string s;
-    init_string(&s);
-
-    curl_easy_setopt(curl, CURLOPT_URL, requestURI);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
-
-    printf("\ncurl options set.");
-    result = curl_easy_perform(curl);
-
-    printf("\nResponse: %s\nResult: %d", s.text, result);
-    
-    strcpy(httpResponse, s.text);
-
-    free(s.text);
-
-    curl_easy_cleanup(curl);
-
-    return httpResponse;
+    printf("\nFailed to allocate text in curl_fetch.");
+    return CURLE_FAILED_INIT;
   }
+
+  response->size = 0;
+
+  curl_easy_setopt(c, CURLOPT_URL, url);
+  curl_easy_setopt(c, CURLOPT_WRITEFUNCTION, curl_callback);
+  curl_easy_setopt(c, CURLOPT_WRITEDATA, (void*)response);
+  
+  result = curl_easy_perform(c);
+
+  return result;
 }
 
 void parseResults(char* response)
@@ -100,25 +86,65 @@ void parseResults(char* response)
   }
 }
 
-void doSearch(char searchTerm[100])
+int doSearch(char *searchTerm)
 {
   char* googCX = "015041558588304470797:lhyjceovlrd";
   char* googAPIKey = "AIzaSyC94Y5BUd9QQ24MlamiR7AS7gUbtkZDcjc";
   char* httpRequest;
-  char* baseURI = "https://www.googleapis.com/customsearch/v1?";
-  char* requestURI;
+  char* baseURI = "https://www.googleapis.com/customsearch/v1?key=AIzaSyC94Y5BUd9QQ24MlamiR7AS7gUbtkZDcjc&cx=015041558588304470797:lhyjceovlrd&q=";
+  char requestURI[1000] = {0};
+  char q[100] = {0};
 
-  sprintf(requestURI, "%skey=%s&cx=%s&q=%s", baseURI, googAPIKey, googCX, searchTerm);
+  snprintf(q, 99, "%s", searchTerm);
+  snprintf(requestURI, 999, "%s%s", baseURI, q);
 
-  //sprintf(httpRequest, "GET %s", requestURI);
+  char* testURI = "https://www.googleapis.com/customsearch/v1?key=AIzaSyC94Y5BUd9QQ24MlamiR7AS7gUbtkZDcjc&cx=015041558588304470797:lhyjceovlrd&q=test";
 
-  printf("\n%s", httpRequest);
+  //printf("\nURL Constructed: %s", requestURI);
+  CURL *c;
+  CURLcode result;
 
-  parseResults(sendRequest(requestURI));
+  struct curl_response curl_response;
+  struct curl_response *cr = &curl_response;
+
+  struct curl_slist *headers = NULL;
+
+  if ((c = curl_easy_init()) == NULL)
+  {
+    printf("\nError initializing curl in doSearch.");
+    return -1;
+  }
+
+  headers = curl_slist_append(headers, "Accept: application/json");
+
+  curl_easy_setopt(c, CURLOPT_CUSTOMREQUEST, "GET");
+  curl_easy_setopt(c, CURLOPT_HTTPHEADER, headers);
+
+  result = curl_fetch(c, requestURI, cr);
+
+  if (result != CURLE_OK || cr->size < 1)
+  {
+    printf("\nError fetching url %s, curl said %s", requestURI, curl_easy_strerror(result));
+    return -2;
+  }
+
+  if (cr->text != NULL)
+  {
+    parseResults(cr->text);
+    free(cr->text);
+    return 0;
+  }
+  else
+  {
+    printf("\nReturned response was null.");
+    free(cr->text);
+    return -3;
+  }
 }
 
 int main() {
   int choice;
+  char* searchString;
 
 
   while(1){
@@ -149,8 +175,9 @@ int main() {
   }
 
   if (choice == 4){
-    printf("search for an item successful\n");
-    doSearch("test");
+    // Whoever does this needs to pass searchString to doSearch()
+    // searchString should be the name of an item the user chooses
+    doSearch(searchString);
   }
 
   if (choice == 5){
